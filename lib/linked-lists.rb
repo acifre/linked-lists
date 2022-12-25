a280803d76b3bced0eae7421ea12265a05e2a43c
acifre <78503928+acifre@users.noreply.github.com>
2022-12-25 15:48:45 -0500

    Add at function

--- lib/linked-lists.rb
+++ lib/linked-lists.rb
@@ -20,7 +20,6 @@ class LinkedList
     @tail = nil
 
   end
-
   def is_empty?
       if @head == nil
           return true
@@ -28,71 +27,93 @@ class LinkedList
           return false
       end
   end
-
-def append(value)
+  def append(value)
   #append(value) adds a new node containing value to the end of the list
   if self.is_empty?
     @head = Node.new(value)
-  else
+  elsif @tail == nil
     @tail = Node.new(value)
     @head.next_node = @tail
-  end
-
-
-end
-
-def prepend(value)
-  #prepend(value) adds a new node containing value to the start of the list
-  if self.is_empty?
-    append(value)
-  elsif @tail == nil
-    @tail = @head
-    @head = Node.new(value, @tail)
-    @tail.next_node = nil
-  elsif @tail != nil
-     new_n = Node.new(value, @head)
-     @head = new_n
-    # have to move everything down by one
-    # so if list only contains head (head not empty, tail empty)
-      # then we need to make current @head the @tail, then make @head the new node
-    # if both head and tail are not empty
-      # then we leave current tail where it is
-      # make new node with next_node current @head value
-      # make new node new @head
-  end
-end
-
-def size
-
-  if self.is_empty?
-    count = 0
   else
-    count = 1
+    new_n = Node.new(value)
+    @tail.next_node = new_n
+    @tail = new_n
+  end
+
+
+  end
+  def prepend(value)
+  #prepend(value) adds a new node containing value to the start of the list
+    if self.is_empty?
+      append(value)
+    elsif @tail == nil
+      @tail = @head
+      @head = Node.new(value, @tail)
+      @tail.next_node = nil
+    elsif @tail != nil
+      new_n = Node.new(value, @head)
+      @head = new_n
+      # have to move everything down by one
+      # so if list only contains head (head not empty, tail empty)
+        # then we need to make current @head the @tail, then make @head the new node
+      # if both head and tail are not empty
+        # then we leave current tail where it is
+        # make new node with next_node current @head value
+        # make new node new @head
+  end
+  end
+  def size
+
+    if self.is_empty?
+      count = 0
+    else
+      count = 1
+      current_node = @head
+
+      while current_node.next_node != nil
+        current_node = current_node.next_node
+        count += 1
+      end
+
+    end
+    count
+  end
+  def head
+    #head returns the first node in the list
+    puts "#{@head} Value: #{@head.value}"
+    @head
+
+  end
+  def tail
+    #tail returns the last node in the list
+    puts "#{@tail} Value: #{@tail.value}"
+    @tail
+
+  end
+
+#at(index) returns the node at the given index
+def at(index)
+
+  if self.is_empty?
+    return "ERROR: list empty"
+  else
+    count = 0
     current_node = @head
+    if count == index
+      return current_node.value
+    end
 
     while current_node.next_node != nil
       current_node = current_node.next_node
       count += 1
+
+      if count == index
+        return current_node.value
+      end
     end
 
   end
-  count
 end
-
-def head
-  #head returns the first node in the list
-  puts "#{@head} Value: #{@head.value}"
-  @head
-
-end
-def tail
-  #tail returns the last node in the list
-  puts "#{@tail} Value: #{@tail.value}"
-  @tail
-
-end
-
-#at(index) returns the node at the given index
 
 #pop removes the last element from the list
 
@@ -117,9 +138,15 @@ new_list.append(5678)
 puts new_list.head
 puts new_list.tail
 
-new_list.prepend(8122)
+new_list.append(8122)
 
 puts new_list.head
 puts new_list.tail
 
 puts new_list.size
+
+new_list.append(1419)
+
+puts new_list.size
+
+puts new_list.at(2)

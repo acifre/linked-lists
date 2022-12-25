dfdf0a677ca753980588b810dc1b6055a7c847bd
acifre <78503928+acifre@users.noreply.github.com>
2022-12-25 17:14:56 -0500

    Refactor and add test file test.rb

--- lib/linked-lists.rb
+++ lib/linked-lists.rb
@@ -167,36 +167,6 @@ class LinkedList
     end
     string += "nil"
   end
-
-
-
 end
 
 
-
-
-new_list = LinkedList.new
-
-new_list.append(1234)
-
-new_list.append(5678)
-
-new_list.append(8122)
-
-new_list.append(1419)
-
-puts new_list.size
-
-# new_list.pop
-
-puts new_list.size
-
-puts new_list.at(0).value
-puts new_list.at(1).value
-puts new_list.at(2).value
-puts new_list.at(3).value
-
-puts new_list.contains?(1419)
-puts new_list.find(1419)
-
-puts new_list.to_s

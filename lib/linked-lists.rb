f5d948ff063d11b6aed911e34146cd8b8515c0a0
acifre <78503928+acifre@users.noreply.github.com>
2022-12-25 14:25:07 -0500

    Add append, head, tail functions to LinkedList

--- lib/linked-lists.rb
+++ lib/linked-lists.rb
@@ -1,26 +1,65 @@
 
+class Node
+
+  attr_accessor :value, :next_node
+
+  def initialize(value=nil, next_node=nil)
+
+    @value = value
+    @next_node = next_node
+  end
+
+end
+
 class LinkedList
 
-  def initialize(head, tail)
+  attr_accessor :name
+
+  def initialize(head=nil, tail=nil)
     @head = nil
     @tail = nil
 
   end
 
-#append(value) adds a new node containing value to the end of the list
+
 
 def append(value)
+  #append(value) adds a new node containing value to the end of the list
+  if @head == nil
+    @head = Node.new(value)
+  else
+    @tail = Node.new(value)
+    @head.next_node = @tail.value
+  end
 
 
 end
 
-#prepend(value) adds a new node containing value to the start of the list
+def prepend(value)
+  #prepend(value) adds a new node containing value to the start of the list
+  if @head == nil
+    append(value)
+  else
+    # have to move everything down by one
+    # so if
+end
 
 #size returns the total number of nodes in the list
 
-#head returns the first node in the list
 
-#tail returns the last node in the list
+
+def head
+  #head returns the first node in the list
+  puts "#{@head} Value: #{@head.value}"
+  @head
+
+end
+def tail
+  #tail returns the last node in the list
+  puts "#{@tail} Value: #{@tail.value}"
+  @tail
+
+end
 
 #at(index) returns the node at the given index
 
@@ -35,19 +74,14 @@ end
 
 end
 
-class Node
-
-  attr_accessor :value, :next_node
-
-  def initialize(value=nil, next_node=nil)
-
-    @value = value
-    @next_node = next_node
-  end
-
-end
 
 
-node = Node.new
 
-puts node.next_node
+new_list = LinkedList.new
+
+new_list.append(1234)
+
+new_list.append(5678)
+
+puts new_list.head
+puts new_list.tail
